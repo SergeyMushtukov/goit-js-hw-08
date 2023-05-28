66eab55b286d5b555c1e0394466b90f7ab3f64f7
SergeyMushtukov <hrystik90@gmail.com>
2023-05-28 18:06:59 +0300

    Выполнил все ДЗ

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframeEl = document.querySelector('#vimeo-player');
+
+const player = new Player(iframeEl);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.setCurrentTime(Number(localStorage.getItem(LOCALSTORAGE_KEY)));
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, `${data.seconds}`);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const itemForUpdate = [...formEl.querySelectorAll('[name]')];
+
+let formData = {};
+
+updateForm();
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  if (itemForUpdate.some(item => item.value === '')) {
+    alert('All the fields must be filled!');
+    return;
+  }
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  console.log(formData);
+  formData = {};
+}
+
+function updateForm() {
+  dataFromLocalStorage = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (dataFromLocalStorage) {
+    formData = JSON.parse(dataFromLocalStorage);
+    itemForUpdate.forEach(item => {
+      if (formData[item.name]) {
+        item.value = formData[item.name];
+      }
+    });
+  }
+}
